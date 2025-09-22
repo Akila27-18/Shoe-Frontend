589a3dca54c84ff6cd5b3fc445ef1292deb4bd15
Akila27-18 <akila271819@gmail.com>
2025-09-22 23:29:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
-import { defineConfig } from 'vite' 
-import react from '@vitejs/plugin-react' 
-import tailwindcss from '@tailwindcss/vite'  
-// https://vite.dev/config/ 
-export default defineConfig({ plugins: [react(), tailwindcss()], })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      // Proxy all requests starting with /api to your backend
+      '/api': {
+        target: 'https://shoe-backend-jbhb.onrender.com',
+        changeOrigin: true,
+        secure: false, // Set to false if using self-signed certs
+      },
+    },
+  },
+});
